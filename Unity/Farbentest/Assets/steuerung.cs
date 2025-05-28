c1f5219c25664f173fba3b1d5ad54ea7b4f4e079
lak7280 <lak7280@thi.de>
2025-05-28 21:27:51 +0200

    Add files via upload

---
+++ Unity/Farbentest/Assets/steuerung.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Steuerung_Rollstuhl : MonoBehaviour
+{
+    public float Speed = 5f;
+    public float SpeedRotation = 100f;
+
+    void Update()
+    {
+        // Vorwärts-/Rückwärtsbewegung (↑/↓)
+        float vorwärts = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        transform.Translate(Vector3.forward * vorwärts);
+
+        // Rotation (←/→)
+        float rotation = Input.GetAxis("Horizontal") * SpeedRotation * Time.deltaTime;
+        transform.Rotate(Vector3.up * rotation);
+    }
+}
